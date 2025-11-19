7a4cb1172b67667459793a48a711bd8d1a92b4ab
Danni Friedland <BlueHotDog@users.noreply.github.com>
2025-11-19 10:37:36 +0100

    adding boilerplate phoenix app

---
+++ apps/frontman_server/test/frontman_server_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule FrontmanServerWeb.ErrorJSONTest do
+  use FrontmanServerWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert FrontmanServerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert FrontmanServerWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
