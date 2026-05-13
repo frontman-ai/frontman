d37a792aea19d9cf9a1d511ec0097a463fa1afad
Danni Friedland <BlueHotDog@users.noreply.github.com>
2026-05-13 14:58:01 +0200

    adding paper_tiger

--- apps/frontman_server/mix.exs
+++ apps/frontman_server/mix.exs
@@ -98,6 +98,7 @@ defmodule FrontmanServer.MixProject do
       # WorkOS for OAuth (GitHub, Google)
       {:workos, "~> 1.1"},
       # ==================DEV/Test=========================
+      {:paper_tiger, "~> 1.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
