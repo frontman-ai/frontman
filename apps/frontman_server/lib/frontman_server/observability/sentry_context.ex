f0d3b7c0672fdd6c3b1ba048487e525b3e97026b
Danni Friedland <BlueHotDog@users.noreply.github.com>
2026-06-24 11:57:01 +0200

    Simplify Sentry scope context

--- apps/frontman_server/lib/frontman_server/observability/sentry_context.ex
+++ apps/frontman_server/lib/frontman_server/observability/sentry_context.ex
@@ -8,7 +8,6 @@ defmodule FrontmanServer.Observability.SentryContext do
   @moduledoc false
 
   alias FrontmanServer.Accounts.Scope
-  alias FrontmanServer.Accounts.User
 
   def set_task_scope_context(%Scope{} = scope, task_id) when is_binary(task_id) do
     set_scope_context(scope)
@@ -19,31 +18,15 @@ defmodule FrontmanServer.Observability.SentryContext do
 
   def set_task_scope_context(_scope, _task_id), do: :ok
 
-  def set_scope_context(%Scope{user: %User{} = user, organization: organization}) do
+  def set_scope_context(%Scope{} = scope) do
     Sentry.Context.set_user_context(%{
-      id: user.id,
-      email: user.email,
-      username: user.name
+      id: Scope.user_id(scope),
+      email: Scope.user_email(scope),
+      username: Scope.user_name(scope)
     })
 
-    extra = %{user_name: user.name}
-
-    extra =
-      case organization do
-        %{id: organization_id, slug: organization_slug} ->
-          Map.merge(extra, %{
-            organization_id: organization_id,
-            organization_slug: organization_slug
-          })
-
-        _ ->
-          extra
-      end
-
-    Sentry.Context.set_extra_context(extra)
-    Sentry.Context.set_tags_context(%{user_id: user.id})
-
-    Logger.metadata(user_id: user.id, user_name: user.name)
+    Sentry.Context.set_tags_context(%{user_id: Scope.user_id(scope)})
+    Logger.metadata(user_id: Scope.user_id(scope), user_name: Scope.user_name(scope))
   end
 
   def set_scope_context(_scope), do: :ok
