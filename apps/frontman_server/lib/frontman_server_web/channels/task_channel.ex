2daff6fda7c31522922ef964cfc59d888edef2a2
uinstinct <61635505+uinstinct@users.noreply.github.com>
2026-08-15 07:47:46 +0000

    notify running in retry_execution

--- apps/frontman_server/lib/frontman_server_web/channels/task_channel.ex
+++ apps/frontman_server/lib/frontman_server_web/channels/task_channel.ex
@@ -283,9 +283,6 @@ defmodule FrontmanServerWeb.TaskChannel do
         _missing -> load_turn_context!(socket, turn_number)
       end
 
-    notification = ACP.build_state_update_notification(socket.assigns.task_id, "running")
-    push(socket, @acp_message, notification)
-
     {:noreply, assign(socket, :active_turn, context)}
   end
 
@@ -789,7 +786,8 @@ defmodule FrontmanServerWeb.TaskChannel do
            }
          ) do
       :ok ->
-        :ok
+        notification = ACP.build_state_update_notification(socket.assigns.task_id, "running")
+        push(socket, @acp_message, notification)
 
       {:error, reason} ->
         unless reason in [:not_found, :stale_turn] do
